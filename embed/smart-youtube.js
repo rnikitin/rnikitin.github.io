8bd6c111e917f9612390d35a020cd29d3c14b579
Roman Nikitin <roma@nikitin.me>
2016-12-06 01:17:43 +0800

    IsMobile added

--- embed/smart-youtube.js
+++ embed/smart-youtube.js
@@ -33,8 +33,14 @@ function onPlayerReady(event) {
 function onPlayerStateChange(event) {
     console.log('youtube state changed:', event.data);
 
-    if (event.data == -1) {
-        window.location = 'vnd.youtube://www.youtube.com/watch?v=' + qs['videoId'] + '&autoplay=1';
+    if (event.data == -1 && isMobile.any()) {
+        // try mobile app activator
+        if (isMobile.Android()){
+            window.location = 'vnd.youtube://www.youtube.com/watch?v=' + qs['videoId'];
+        }
+        else {
+            window.location = 'http://www.youtube.com/watch?v=' + qs['videoId'] + '&autoplay=1';
+        }
     }
 }
 function stopVideo() {
@@ -46,3 +52,24 @@ function getQueryString(){
     location.search.substr(1).split("&").forEach(function(item) {(item.split("=")[0] in qd) ? qd[item.split("=")[0]].push(item.split("=")[1]) : qd[item.split("=")[0]] = [item.split("=")[1]]})
     return qd;
 }
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
